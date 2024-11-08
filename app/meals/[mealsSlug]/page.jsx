e50d97e4a9bf51d4514c894d97edd5688292d637
hadienzz <122720958+hadienzz@users.noreply.github.com>
2024-11-08 21:52:07 +0700

    asdas
    dsadad

---
+++ app/meals/[mealsSlug]/page.jsx
@@ -0,0 +1,9 @@
+const MealDetail = ({ params }) => {
+  return (
+    <main>
+      <h1>This is {params.mealsSlug} page</h1>
+    </main>
+  );
+};
+
+export default MealDetail;
---
+++ app/meals/share/page.jsx
@@ -0,0 +1,11 @@
+const PageShare = () => {
+  return (
+
+    
+    <main>
+      <h1>This is share page</h1>
+    </main>
+  );
+};
+
+export default PageShare;
